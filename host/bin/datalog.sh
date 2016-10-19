8b9c9c6ae6f5a392f9162fbf435d8d37431c8296
Andrew Howat <andrew@whisperinggibbon.com>
2016-10-19 11:52:46 +0100

    Data log service - initial import Takes a snapshot every 15s of the pi's memory and temperature. Accepts user submitted args for memory warning, memory error, temperature warning and temperature error threshold values, and can run custom code based on that. Will also increase the granularity (currently 1s) of logging based on if a warning/error is triggered until the value is under threshold again.
    Script can be ran standalone on an affected pi by using ./datalog.sh <memory warning threshold in kb> <memory error threshold in kb> <cpu temperature warning threshold in Celsius> <cpu temperature error threshold in Celsius>
    [args are optional but recommended for maximum effectiveness]

---
+++ host/bin/datalog.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#args:
+# $1 - memory warning
+# $2 - memory error
+# $3 - temperature warning
+# $4 - temperature error
+
+filename=log-$(date "+%y-%m-%d").dat
+touch $filename
+
+defaultsleeptime=15s
+#parameterising so that the script can log more regularly if warnings are triggered
+changes=0
+sleeptime=$defaultsleeptime
+while true; do
+	process=$(ps -A | grep java |awk '{$2=$2};1'|cut -d " " -f1)
+	mem=$(cat /proc/$process/status|grep VmSize)
+	max=$(cat /proc/$process/status|grep VmPeak)
+
+	timestamp=$(date "+%d/%m/%y %H:%M:%S")
+
+	cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
+	cpuTemp1=$(($cpuTemp0/1000))
+	cpuTemp2=$(($cpuTemp0/100))
+	cpuTempM=$(($cpuTemp2 % $cpuTemp1))
+
+	gpuTemp0=$(/opt/vc/bin/vcgencmd measure_temp)
+	gpuTemp0=${gpuTemp0//\'/º}
+	gpuTemp0=${gpuTemp0//temp=/}
+	timestamp=$(date "+%d/%m/%y %H:%M:%S")
+	echo "["$timestamp"] Curr mem: "$mem", Max Mem: "$max". CPU Temp: "$cpuTemp1"."$cpuTempM"ºC GPU Temp: "$gpuTemp0 >> $filename
+	
+	#do error checking
+
+#	if [ ! -z "$1" ]; then
+#		#changes=$changes + 1
+#	fi
+
+#	if [ ! -z "$2" ]; then
+#		#changes=$changes + 1
+#	fi
+
+	if [ ! -z "$3" ]; then
+		if [ $(bc <<< "$3 <= $cpuTemp1.$cpuTempM") -eq 1 ]; then
+			warnstring="WARNING! CPU Temperature is too high! ($cpuTemp1.$cpuTempM)"
+			let changes=changes+1
+		fi
+	fi
+
+	if [ ! -z "$4" ]; then
+		if [ $(bc <<< "$4 <= $cpuTemp1.$cpuTempM") -eq 1 ]; then
+			warnstring="ERROR! CPU Temperature is too high ($cpuTemp1.$cpuTempM)"
+			let changes=changes+1
+		fi
+	fi
+
+	if [ "$changes" -gt "0" ]; then
+		echo "["$timestamp"] "$warnstring
+		sleeptime=1s
+	else
+		sleeptime=$defaultsleeptime
+	fi
+	changes=0
+	sleep $sleeptime
+done
