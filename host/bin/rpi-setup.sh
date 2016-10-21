6ffbe34a58a2f195076524f58f0f018e2e9140f8
WGAndrew <andrew@whisperinggibbon.com>
2016-10-21 13:43:02 +0100

    Refactored to require superuser

--- host/bin/rpi-setup.sh
+++ host/bin/rpi-setup.sh
@@ -11,18 +11,25 @@ export newpassword=photocentric
 # Ideally the password should be changed offline, and this repo should never be updated to match that.
 # but it's still better than sending out the hardware with the default pi user and password unchanged. Obviously.
 
+#require SU
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
 
 # DO FOR ALL
 echo "Getting updates and installing utilities"
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install rpi-chromium-mods dos2unix curl librxtx-java fbi git rsync rpi-update matchbox-window-manager uzbl xinit nodm Xorg unclutter feh jq tint2 wmctrl
+apt-get update
+apt-get -y upgrade
+apt-get -y install rpi-chromium-mods dos2unix curl librxtx-java fbi git rsync rpi-update matchbox-window-manager uzbl xinit nodm Xorg unclutter feh jq tint2 wmctrl
 
 if [ -e photonic-repo ]; then
-	sudo rm -rf photonic-repo
+	rm -rf photonic-repo
 fi
-sudo git clone https://github.com/Photocentric3D/Photonic3D.git photonic-repo
-sudo rpi-update
+git clone https://github.com/Photocentric3D/Photonic3D.git photonic-repo
+rpi-update
 
 if [[ "[ "$newhost" == "4kscreen" ]" || "[ "$newhost" == "LCHR" ]" || "[ "$newhost" == "standalone" ]" ]]
 	then
@@ -30,59 +37,59 @@ if [[ "[ "$newhost" == "4kscreen" ]" || "[ "$newhost" == "LCHR" ]" || "[ "$newho
 		# would prefer to call this to update to a particular version,
 		# but the auto-update in start.sh will flatten any changes we make in the next section
 		# so doing this first.
-		sudo chmod +x photonic-repo/host/bin/*.sh
-		sudo dos2unix photonic-repo/host/bin/*.sh
+		chmod +x photonic-repo/host/bin/*.sh
+		dos2unix photonic-repo/host/bin/*.sh
 		if [ -e "/opt/cwh/stop.sh" ]
 			then
 				# kill the old version of photonic using its stop.sh script
-				sudo /opt/cwh/stop.sh
+				/opt/cwh/stop.sh
 		fi
 		# launch the Photocentric rather than area515 version of start.sh - ensures using Photocentric branch
-		sudo photonic-repo/host/bin/start.sh
+		photonic-repo/host/bin/start.sh
 fi
-sudo dos2unix /opt/cwh/*.sh
+dos2unix /opt/cwh/*.sh
 
 # redirect boot terminal output not to screen
 echo "removing pi branding"
 # ensure the pi always boots to console
-sudo raspi-config nonint do_boot_behaviour B2
+raspi-config nonint do_boot_behaviour B2
 # we can't remove everything as some is baked into the pi's firmware, but this gives a realistic amount.
 if [ ! -e "/boot/cmdline.old" ]; then
-	sudo mv /boot/cmdline.txt /boot/cmdline.old 
-	sudo sh -c 'echo -n "loglevel=3 logo.nologo " > /boot/cmdline.txt'
-	sudo sh -c 'cat /boot/cmdline.old >> /boot/cmdline.txt' 
-	sudo sed -i "s/tty1/tty/g" /boot/cmdline.txt
+	mv /boot/cmdline.txt /boot/cmdline.old 
+	echo -n "loglevel=3 logo.nologo " > /boot/cmdline.txt
+	cat /boot/cmdline.old >> /boot/cmdline.txt 
+	sed -i "s/tty1/tty/g" /boot/cmdline.txt
 else
 	echo "already complete!"
 fi
 
 echo "installing common files"
-sudo rsync -avr photonic-repo/host/common/ /
-sudo rsync -avr photonic-repo/host/resourcesnew/printflow /opt/cwh/resourcesnew/ #keep printflow without the trailing / 
+rsync -avr photonic-repo/host/common/ /
+rsync -avr photonic-repo/host/resourcesnew/printflow /opt/cwh/resourcesnew/ #keep printflow without the trailing / 
 if [ -e /opt/cwh/photocentric/ ]; then
-	sudo mkdir /opt/cwh/photocentric
+	mkdir /opt/cwh/photocentric
 fi
-sudo rsync -avr photonic-repo/host/photocentric/printflow /opt/cwh/photocentric/
-sudo cp photonic-repo/host/os/Linux/armv61/pdp /opt/cwh/os/Linux/armv61/pdp #copy display manager for screen + curing screen printers
-sudo cp photonic-repo/host/resourcesnew/printflow/holdingpage.html /home/pi/holdingpage.html #copy holdingpage for fallback
+rsync -avr photonic-repo/host/photocentric/printflow /opt/cwh/photocentric/
+cp photonic-repo/host/os/Linux/armv61/pdp /opt/cwh/os/Linux/armv61/pdp #copy display manager for screen + curing screen printers
+cp photonic-repo/host/resourcesnew/printflow/holdingpage.html /home/pi/holdingpage.html #copy holdingpage for fallback
 #install splash screen
-sudo chown root /etc/splash.png
-sudo chmod 777 /etc/splash.png
-sudo chmod a+x /etc/init.d/aasplashscreen
-sudo insserv /etc/init.d/aasplashscreen
+chown root /etc/splash.png
+chmod 777 /etc/splash.png
+chmod a+x /etc/init.d/aasplashscreen
+insserv /etc/init.d/aasplashscreen
 
 echo "disabling screen power down"
 if [ -e "/etc/default/nodm" ]
 	then
-		sudo sed -i "s/=false/=true/g" /etc/default/nodm
-		sudo sed -i "s/root/pi/g" /etc/default/nodm
+		sed -i "s/=false/=true/g" /etc/default/nodm
+		sed -i "s/root/pi/g" /etc/default/nodm
 	else
 		echo "nodm doesn't exist"
 fi
 # this already happens once due to our rsync of an X11 config, but why not just make sure, eh?
 
 echo "Working on per printer settings..."
-sudo sh -c 'echo \# Photocentric mods >> /boot/config.txt'
+echo \# Photocentric mods >> /boot/config.txt
 
 if [[ "[ "$newhost" == "4ktouch" ]" || "[ "$newhost" == "LCHR" ]" || "[ "$newhost" == "standalone" ]" ]]; then
 	# Touchscreen pis only
@@ -91,9 +98,9 @@ if [[ "[ "$newhost" == "4ktouch" ]" || "[ "$newhost" == "LCHR" ]" || "[ "$newhos
 	then
 		echo "already done!"
 	else
-		sudo sh -c 'echo disable_splash=1 >> /boot/config.txt'
-		sudo sh -c 'echo lcd_rotate=2 >> /boot/config.txt'
-		sudo sh -c 'echo avoid_warnings=1 >> /boot/config.txt'
+		echo disable_splash=1 >> /boot/config.txt
+		echo lcd_rotate=2 >> /boot/config.txt
+		echo avoid_warnings=1 >> /boot/config.txt
 	fi
 	echo "installing kiosk browser"
 	# since this isn't on standard apt-get sources, i'm keeping this as an alternative way to source kweb
@@ -106,10 +113,10 @@ if [[ "[ "$newhost" == "4ktouch" ]" || "[ "$newhost" == "LCHR" ]" || "[ "$newhos
 	then
 		echo "already done!"
 	else
-		sudo wget -qO - http://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
-		sudo sh -c 'echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" | sudo tee -a /etc/apt/sources.list'
-		sudo apt-get update
-		sudo sh -c "yes Y | apt-get install -y kweb"
+		wget -qO - http://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
+		echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" | tee -a /etc/apt/sources.list
+		apt-get update
+		yes Y | apt-get install -y kweb
 	fi
 	
 	touch /home/pi/.xsession
@@ -161,9 +168,9 @@ if [ "$newhost" == "4ktouch" ]
 
 		#enabling NTP listening 
 		echo setting up network time client
-		sudo sed -i "s/\#disable/disable/g" /etc/ntp.conf
-		sudo sed -i "s/\#broadcastclient/broadcastclient/g" /etc/ntp.conf
-		sudo /etc/init.d/ntp restart
+		sed -i "s/\#disable/disable/g" /etc/ntp.conf
+		sed -i "s/\#broadcastclient/broadcastclient/g" /etc/ntp.conf
+		/etc/init.d/ntp restart
 fi
 
 if [ "$newhost" == "4kscreen" ]
@@ -173,19 +180,19 @@ if [ "$newhost" == "4kscreen" ]
 			then
 				echo "already done!"
 			else
-				sudo sh -c 'echo hdmi_group=2 >> /boot/config.txt'
-				sudo sh -c 'echo hdmi_mode=87 >> /boot/config.txt'
-				sudo sh -c 'echo hdmi_cvt 3840 2160 24 >> /boot/config.txt'
-				sudo sh -c 'echo max_framebuffer_width=3840 >> /boot/config.txt'
-				sudo sh -c 'echo max_framebuffer_height=2160 >> /boot/config.txt'
-				sudo sh -c 'echo hdmi_pixel_freq_limit=400000000 >> /boot/config.txt'
+				echo hdmi_group=2 >> /boot/config.txt
+				echo hdmi_mode=87 >> /boot/config.txt
+				echo hdmi_cvt 3840 2160 24 >> /boot/config.txt
+				echo max_framebuffer_width=3840 >> /boot/config.txt
+				echo max_framebuffer_height=2160 >> /boot/config.txt
+				echo hdmi_pixel_freq_limit=400000000 >> /boot/config.txt
 		fi
 		
 		#TODO - add network time propogation to support 4ktouch. Currently built into WG images, but not setup by shell script yet
 		echo "setting up Photocentric Pro profile"
-		sudo wget https://raw.githubusercontent.com/Photocentric3D/Photonic3D/master/host/printers/Photocentric%20Pro.json -O printerprofile.json
-		sudo curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @printerprofile.json 'http://localhost:$portno/services/printers/save'
-		sudo sh -c 'echo var printerName = \"Photocentric Pro\"\; > /opt/cwh/resourcesnew/printflow/js/printerconfig.js'
+		wget https://raw.githubusercontent.com/Photocentric3D/Photonic3D/master/host/printers/Photocentric%20Pro.json -O printerprofile.json
+		curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @printerprofile.json 'http://localhost:$portno/services/printers/save'
+		echo var printerName = \"Photocentric Pro\"\; > /opt/cwh/resourcesnew/printflow/js/printerconfig.js
 fi
 
 if [ "$newhost" == "LCHR" ]
@@ -195,19 +202,19 @@ if [ "$newhost" == "LCHR" ]
 			then
 				echo "already done!"
 			else
-				sudo sh -c 'echo hdmi_group=2 >> /boot/config.txt'
-				sudo sh -c 'echo hdmi_mode=87 >> /boot/config.txt'
-				sudo sh -c 'echo hdmi_cvt 2048 1536 30 >> /boot/config.txt'
-				sudo sh -c 'echo max_framebuffer_width=2048 >> /boot/config.txt'
-				sudo sh -c 'echo max_framebuffer_height=1536 >> /boot/config.txt'
-				sudo sh -c 'echo hdmi_pixel_freq_limit=400000000 >> /boot/config.txt'
+				echo hdmi_group=2 >> /boot/config.txt
+				echo hdmi_mode=87 >> /boot/config.txt
+				echo hdmi_cvt 2048 1536 30 >> /boot/config.txt
+				echo max_framebuffer_width=2048 >> /boot/config.txt
+				echo max_framebuffer_height=1536 >> /boot/config.txt
+				echo hdmi_pixel_freq_limit=400000000 >> /boot/config.txt
 		fi
 
 
 		echo "installing Photocentric Liquid Crystal HR profile"
-		sudo wget https://raw.githubusercontent.com/Photocentric3D/Photonic3D/master/host/printers/photocentric%20hr.json -O printerprofile.json
-		sudo curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @printerprofile.json 'http://localhost:$portno/services/printers/save'
-		sudo sh -c 'echo var printerName = \"LC HR\"\; > /opt/cwh/resourcesnew/printflow/js/printerconfig.js'
+		wget https://raw.githubusercontent.com/Photocentric3D/Photonic3D/master/host/printers/photocentric%20hr.json -O printerprofile.json
+		curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @printerprofile.json 'http://localhost:$portno/services/printers/save'
+		echo var printerName = \"LC HR\"\; > /opt/cwh/resourcesnew/printflow/js/printerconfig.js
 fi
 
 
@@ -216,26 +223,26 @@ if [ "$newhost" == "standalone" ]
 		echo "creating standalone image..."
 		#TODO
 		echo "installing Photocentric 10 profile"
-		sudo wget https://raw.githubusercontent.com/Photocentric3D/Photonic3D/master/host/printers/photocentric%2010.json -O printerprofile.json
-		sudo curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @printerprofile.json "http://localhost:$portno/services/printers/save"
-		sudo sh -c 'echo var printerName = \"Photocentric 10\"\; > /opt/cwh/resourcesnew/printflow/js/printerconfig.js'
+		wget https://raw.githubusercontent.com/Photocentric3D/Photonic3D/master/host/printers/photocentric%2010.json -O printerprofile.json
+		curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @printerprofile.json "http://localhost:$portno/services/printers/save"
+		echo var printerName = \"Photocentric 10\"\; > /opt/cwh/resourcesnew/printflow/js/printerconfig.js
 fi
 
 # Change hostname
 # left this 'til last for good reasons. Keep it last now.
 export hostn=$(cat /etc/hostname)
 echo "Existing hostname is $hostn, changing to $newhost"
-sudo sed -i "s/$hostn/$newhost/g" /etc/hosts
-sudo sed -i "s/$hostn/$newhost/g" /etc/hostname
+sed -i "s/$hostn/$newhost/g" /etc/hosts
+sed -i "s/$hostn/$newhost/g" /etc/hostname
 echo "Your new hostname is $newhost, accessible from $newhost.local"
 
 echo "changing password"
-sudo sh -c "echo 'pi:$newpassword' | chpasswd"
+echo 'pi:$newpassword' | chpasswd
 echo "password updated!"
 #if you haven't already, re-read the big important note at the top!
 
-sudo rm -rf photonic-repo
-sudo rm printerprofile.json
+rm -rf photonic-repo
+rm printerprofile.json
 
-sudo apt-get clean
-sudo reboot
+apt-get clean
+reboot
