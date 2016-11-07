f301a18424dbc0a72eec0ec03234ea81d2416f83
Andrew Howat <andrew@whisperinggibbon.com>
2016-11-07 10:47:50 +0000

    chmods pdp after compilation

--- host/src/main/c/build.sh
+++ host/src/main/c/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 gcc -I /opt/vc/include -L /opt/vc/lib -O2 -o pdp -lbcm_host pdp.c
+if [ -e "pdp" ]; then chmod +x pdp; fi
 #gcc -I /opt/vc/include -L /opt/vc/lib -o dispmanx -lbcm_host dispmanx.c
 #gcc -I /opt/vc/include -L /opt/vc/lib -O2 -o makeimage makeimage.c
