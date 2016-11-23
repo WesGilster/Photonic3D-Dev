03889c31c0e59e8a838de753ae8f89f79e2609ea
WGAndrew <andrew@whisperinggibbon.com>
2016-11-23 11:42:49 +0000

    Improvement for start.sh not auto-updating on boot
    This now tests for a connection to github. It will wait for up to 20 seconds [tunable on line 105] for a network connection.
    
    If it's found, it will do all the logic for online updating. 
    If it isn't found, it'll push ahead with launching Photonic.
    
    This will introduce some extra time into the raspberry pi boot sequence, but as the DHCP information isn't available 99% of the times when this script is reaching a stage that needs it, it's better to do it this way and enable online updating.

--- host/bin/start.sh
+++ host/bin/start.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+##### FUNCTIONS #####
+
+function networkup {
+  # Initialize number of attempts
+  reachable=$1
+  while [ $reachable -ne 0 ]; do
+    # Ping supplied host
+    ping -q -c 1 -W 1 "$2" > /dev/null 2>&1
+    # Check return code
+    if [ $? -eq 0 ]; then
+      # Success, we can exit with the right return code
+      echo 0
+      return
+    fi
+    # Network down, decrement counter and try again
+    let reachable-=1
+    # Sleep for one second
+    sleep 1
+  done
+  # Network down, number of attempts exhausted, quiting
+  echo 1
+}
+
+##### MAIN #####
+
 # require superuser
 if [[ $UID != 0 ]]; then
     echo "Please run this script with sudo:"
@@ -68,11 +93,6 @@ elif [ "${cpu}" = "aarch64" ]; then
 	javaURL="http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-arm64-vfp-hflt.tar.gz"
 fi
 
-if [ ! -f "/usr/lib/jni/librxtxSerial.so" ]; then
-	echo Installing RxTx
-	apt-get install --yes --force-yes librxtx-java
-fi
-
 #This application will always need to have the display set to the following
 export DISPLAY=:0.0
 xinitProcess=`ps -ef | grep grep -v | grep xinit`
@@ -82,100 +102,110 @@ if [ -z "${xinitProcess}" ]; then
     xhost +x
 fi
 
-#Copy the zip file from the current directory into the cwh directory for offline install
-mkdir -p ${installDirectory}
-mv ${downloadPrefix}.*.zip ${installDirectory}
-
-#install java if version is too old
-javaInstalled=`which java`
-if [ "$javaInstalled" = "" ]; then
-	javaMajorVersion=0
-	javaMinorVersion=0
-else
-	javaMajorVersion=`java -version 2>&1 | grep "java version" | awk -F[\".] '{print "0"$2}'`
-	javaMinorVersion=`java -version 2>&1 | grep "java version" | awk -F[\".] '{print "0"$3}'`
-fi
-
-if [ "$javaMinorVersion" -lt 8 -a "$javaMajorVersion" -le 1 ]; then
-	downloadJavaFile=`echo ${javaURL} | awk -F/ '{print $(NF)}'`
-	echo Either Java is not installed, or an incorrect version of Java is installed. Installing from this URL: ${javaURL}
-	mkdir -p /usr/lib/jvm
-	cd /usr/lib/jvm
-	rm ${downloadJavaFile}
-	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "${javaURL}"
-
-	firstSnapshot=`ls -1`
-	echo Unzipping and installing Java now
-	tar xzf ${downloadJavaFile}
-	secondSnapshot=`ls -1`
-	javaInstallFile=`echo "$firstSnapshot"$'\n'"$secondSnapshot" | sort | uniq -u`
-
-	if [ -z "${javaInstallFile}" ]; then
-		echo "A new version of Java is available, please update this script with the proper download URLS from: http://www.oracle.com/technetwork/java/javase/downloads/index.html"
-		exit
+if [ $(networkup 20 www.github.com) -eq 1 ]; then
+	echo "unable to update, no network connection. Starting Photonic3D OFFLINE"
+else 
+	if [ ! -f "/usr/lib/jni/librxtxSerial.so" ]; then
+		echo Installing RxTx
+		apt-get install --yes --force-yes librxtx-java
 	fi
 
-	ln -sf /usr/lib/jvm/${javaInstallFile}/bin/java /usr/bin/java
-	ln -sf /usr/lib/jvm/${javaInstallFile}/bin/javac /usr/bin/javac
-	ln -sf /usr/lib/jvm/${javaInstallFile}/bin/keytool /usr/bin/keytool
-	rm ${downloadJavaFile}
-fi
-
-#Determine if a new install is available
-echo Checking for new version from Github Repo: ${repo}
-cd ${installDirectory}
-LOCAL_TAG=$(grep repo.version build.number | cut -d = -f 2 | tr -d '\r')
-NETWORK_TAG=$(curl -L -s https://api.github.com/repos/${repo}/releases/latest | grep 'tag_name' | cut -d\" -f4)
-
-echo Local Tag: ${LOCAL_TAG}
-echo Network Tag: ${NETWORK_TAG}
-
-if [ -f ${downloadPrefix}.*.zip ]; then
-	OFFLINE_FILE=$(ls ${downloadPrefix}.*.zip)
-	echo Performing offline install of ${OFFLINE_FILE}
-
-	mv ${OFFLINE_FILE} ~
-	rm -r ${installDirectory}
+	#Copy the zip file from the current directory into the cwh directory for offline install
 	mkdir -p ${installDirectory}
-	cd ${installDirectory}
-	mv ~/${OFFLINE_FILE} .
-	unzip ${OFFLINE_FILE}
-	chmod 777 *.sh
-	rm ${OFFLINE_FILE}
-elif [ -z "${NETWORK_TAG}" ]; then
-	echo "Couldn't fetch version from GitHub, launching existing install."
-elif [ "${NETWORK_TAG}" != "${LOCAL_TAG}" -o "$2" == "force" ]; then
-	echo Installing latest version of ${downloadPrefix}: ${NETWORK_TAG}
+	mv ${downloadPrefix}.*.zip ${installDirectory}
 
-	DL_URL=$(curl -L -s https://api.github.com/repos/${repo}/releases/latest | grep 'browser_' | cut -d\" -f4 | grep -- -${NETWORK_TAG})
-	DL_FILE=${DL_URL##*/}
-	rm -f "/tmp/${DL_FILE}"
-	wget -P /tmp "${DL_URL}"
-	if [ $? -ne 0 ]; then
-		echo "wget of ${DL_FILE} failed. Aborting update."
-		exit 1
+	#install java if version is too old
+	javaInstalled=`which java`
+	if [ "$javaInstalled" = "" ]; then
+		javaMajorVersion=0
+		javaMinorVersion=0
+	else
+		javaMajorVersion=`java -version 2>&1 | grep "java version" | awk -F[\".] '{print "0"$2}'`
+		javaMinorVersion=`java -version 2>&1 | grep "java version" | awk -F[\".] '{print "0"$3}'`
 	fi
 
+	if [ "$javaMinorVersion" -lt 8 -a "$javaMajorVersion" -le 1 ]; then
+		downloadJavaFile=`echo ${javaURL} | awk -F/ '{print $(NF)}'`
+		echo Either Java is not installed, or an incorrect version of Java is installed. Installing from this URL: ${javaURL}
+		mkdir -p /usr/lib/jvm
+		cd /usr/lib/jvm
+		rm ${downloadJavaFile}
+		wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "${javaURL}"
 
-	
-	rm -r ${installDirectory}
-	mkdir -p ${installDirectory}
+		firstSnapshot=`ls -1`
+		echo Unzipping and installing Java now
+		tar xzf ${downloadJavaFile}
+		secondSnapshot=`ls -1`
+		javaInstallFile=`echo "$firstSnapshot"$'\n'"$secondSnapshot" | sort | uniq -u`
+
+		if [ -z "${javaInstallFile}" ]; then
+			echo "A new version of Java is available, please update this script with the proper download URLS from: http://www.oracle.com/technetwork/java/javase/downloads/index.html"
+			exit
+		fi
+
+		ln -sf /usr/lib/jvm/${javaInstallFile}/bin/java /usr/bin/java
+		ln -sf /usr/lib/jvm/${javaInstallFile}/bin/javac /usr/bin/javac
+		ln -sf /usr/lib/jvm/${javaInstallFile}/bin/keytool /usr/bin/keytool
+		rm ${downloadJavaFile}
+	fi
+
+	#Determine if a new install is available
+	echo Checking for new version from Github Repo: ${repo}
 	cd ${installDirectory}
-	mv "/tmp/${DL_FILE}" .
+	LOCAL_TAG=$(grep repo.version build.number | cut -d = -f 2 | tr -d '\r')
+	NETWORK_TAG=$(curl -L -s https://api.github.com/repos/${repo}/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
-	unzip ${DL_FILE}
-	
-	chmod 777 *.sh
-	# grab dos2unix from the package manager if not installed
-	command -v dos2unix >/dev/null 2>&1 || { apt-get install --yes --force-yes dos2unix >&2; }
-	grep -lU $'\x0D' *.sh | xargs dos2unix
-	# ensure the cwhservice always is linux format and executable
-	grep -lU $'\x0D' /etc/init.d/cwhservice | xargs dos2unix
-	chmod +x /etc/init.d/cwhservice
-	chmod +x /opt/cwh/os/Linux/armv61/pdp
-	rm ${DL_FILE}
-else
-	echo No install required
+	echo Local Tag: ${LOCAL_TAG}
+	echo Network Tag: ${NETWORK_TAG}
+
+	if [ -f ${downloadPrefix}.*.zip ]; then
+		OFFLINE_FILE=$(ls ${downloadPrefix}.*.zip)
+		echo Performing offline install of ${OFFLINE_FILE}
+
+		mv ${OFFLINE_FILE} ~
+		rm -r ${installDirectory}
+		mkdir -p ${installDirectory}
+		cd ${installDirectory}
+		mv ~/${OFFLINE_FILE} .
+		unzip ${OFFLINE_FILE}
+		chmod 777 *.sh
+		rm ${OFFLINE_FILE}
+	elif [ -z "${NETWORK_TAG}" ]; then
+		echo "Couldn't fetch version from GitHub, launching existing install."
+	elif [ "${NETWORK_TAG}" != "${LOCAL_TAG}" -o "$2" == "force" ]; then
+		echo Installing latest version of ${downloadPrefix}: ${NETWORK_TAG}
+
+		DL_URL=$(curl -L -s https://api.github.com/repos/${repo}/releases/latest | grep 'browser_' | cut -d\" -f4 | grep -- -${NETWORK_TAG})
+		DL_FILE=${DL_URL##*/}
+		rm -f "/tmp/${DL_FILE}"
+		wget -P /tmp "${DL_URL}"
+		if [ $? -ne 0 ]; then
+			echo "wget of ${DL_FILE} failed. Aborting update."
+			exit 1
+		fi
+
+
+
+		rm -r ${installDirectory}
+		mkdir -p ${installDirectory}
+		cd ${installDirectory}
+		mv "/tmp/${DL_FILE}" .
+
+		unzip ${DL_FILE}
+
+		chmod 777 *.sh
+		# grab dos2unix from the package manager if not installed
+		command -v dos2unix >/dev/null 2>&1 || { apt-get install --yes --force-yes dos2unix >&2; }
+		grep -lU $'\x0D' *.sh | xargs dos2unix
+		# ensure the cwhservice always is linux format and executable
+		grep -lU $'\x0D' /etc/init.d/cwhservice | xargs dos2unix
+		chmod +x /etc/init.d/cwhservice
+		chmod +x /opt/cwh/os/Linux/armv61/pdp
+		rm ${DL_FILE}
+	else
+		echo No install required
+	fi
+  
 fi
 
 
